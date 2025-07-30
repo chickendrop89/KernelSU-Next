0f935ef3167496edf81d66a8b259013964b2cb82
chickendrop89 <chickendrop89@gmail.com>
2025-07-30 23:15:58 +0200

    Revert "kernel: core_hook: fix a typo"
    This reverts commit cdd2766cbcd2d0c72f5f3ed9e72fff18b5fb0ef0.

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -1127,7 +1127,7 @@ out_ksu_try_umount:
 	ksu_try_umount("/sbin", false, MNT_DETACH);
 
 	// try umount hosts file
-	ksu_try_umount("/system/etc/hosts", false, MNT_DETACH);
+	ksu_ry_umount("/system/etc/hosts", false, MNT_DETACH);
 
 	// try umount lsposed dex2oat bins
 	ksu_try_umount("/apex/com.android.art/bin/dex2oat64", false, MNT_DETACH);
